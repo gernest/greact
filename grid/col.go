7b2067874e49a95d0299d305e926304526f830c2
gernest <geofreyernest@live.com>
2018-04-29 15:07:07 +0300

    complete COlumn component

--- grid/col.go
+++ grid/col.go
@@ -1,8 +1,11 @@
 package grid
 
 import (
+	"github.com/gernest/gs"
 	"github.com/gernest/vected/style/grid"
+	"github.com/gernest/vected/ui"
 	"github.com/gopherjs/vecty"
+	"github.com/gopherjs/vecty/elem"
 )
 
 type Number = grid.Number
@@ -39,15 +42,68 @@ type ColOptions = grid.ColOptions
 
 type Column struct {
 	vecty.Core
-	ColOptions
+	*ColOptions
 
 	Style                   vecty.Applyer
 	Children                vecty.MarkupOrChild
 	XS, SM, MD, LG, XL, XXL *ColOptions
+
+	sheet *gs.Sheet
+}
+
+func (c *Column) Mount() {
+	c.sheet.Attach()
 }
 
 func (c *Column) Render() vecty.ComponentOrHTML {
-	return nil
+	if c.sheet == nil {
+		c.sheet = ui.NewSheet()
+		c.sheet.AddRule(c.style())
+	}
+	classes := vecty.ClassMap(c.sheet.CLasses.Classes())
+	return elem.Div(vecty.Markup(classes, c.Style), c.Children)
+}
+
+func (c *Column) style() gs.CSSRule {
+	var media []grid.MediaOption
+	if c.XS != nil {
+		media = append(media, grid.MediaOption{
+			Type: grid.XS,
+			Opts: c.XS,
+		})
+	}
+	if c.SM != nil {
+		media = append(media, grid.MediaOption{
+			Type: grid.SM,
+			Opts: c.SM,
+		})
+	}
+	if c.MD != nil {
+		media = append(media, grid.MediaOption{
+			Type: grid.MD,
+			Opts: c.MD,
+		})
+	}
+	if c.LG != nil {
+		media = append(media, grid.MediaOption{
+			Type: grid.LG,
+			Opts: c.LG,
+		})
+	}
+
+	if c.XL != nil {
+		media = append(media, grid.MediaOption{
+			Type: grid.XL,
+			Opts: c.XL,
+		})
+	}
+	if c.XXL != nil {
+		media = append(media, grid.MediaOption{
+			Type: grid.XXL,
+			Opts: c.XXL,
+		})
+	}
+	return grid.Column(c.ColOptions, media...)
 }
 
 func join(s ...string) string {
@@ -58,6 +114,6 @@ func join(s ...string) string {
 	return o
 }
 
-type Row struct {
-	vecty.Core
+func (c *Column) Unmount() {
+	c.sheet.Detach()
 }

5892e5d0c0532e08790c514da3fa66f982b3f7ae
gernest <geofreyernest@live.com>
2018-04-30 16:13:22 +0300

    base render

--- grid/demo/components/basic_grid.go
+++ grid/demo/components/basic_grid.go
@@ -1,13 +1,54 @@
 package components
 
 import (
+	"github.com/gernest/vected/grid"
 	"github.com/gopherjs/vecty"
+	"github.com/gopherjs/vecty/elem"
 )
 
 type BasicGrid struct {
 	vecty.Core
 }
 
+func BasicGridRow(span grid.Number) vecty.Component {
+	return &grid.Column{
+		Span: span,
+		Children: func() vecty.MarkupOrChild {
+			return vecty.Text(span.String())
+		},
+	}
+}
 func (BasicGrid) Render() vecty.ComponentOrHTML {
-	return nil
+	return elem.Div(
+		&grid.Row{
+			CSS: codeBoxDemo(),
+			Children: func() vecty.MarkupOrChild {
+				return vecty.List{
+					BasicGridRow(grid.G12),
+					BasicGridRow(grid.G12),
+				}
+			},
+		},
+		&grid.Row{
+			CSS: codeBoxDemo(),
+			Children: func() vecty.MarkupOrChild {
+				return vecty.List{
+					BasicGridRow(grid.G8),
+					BasicGridRow(grid.G8),
+					BasicGridRow(grid.G8),
+				}
+			},
+		},
+		&grid.Row{
+			CSS: codeBoxDemo(),
+			Children: func() vecty.MarkupOrChild {
+				return vecty.List{
+					BasicGridRow(grid.G6),
+					BasicGridRow(grid.G6),
+					BasicGridRow(grid.G6),
+					BasicGridRow(grid.G6),
+				}
+			},
+		},
+	)
 }

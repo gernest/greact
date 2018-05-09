7f29ae17b9ba39a92dbc5d20907f1db6c3bd24e6
gernest <geofreyernest@live.com>
2018-05-09 13:29:09 +0300

    add streamResponse

---
+++ cmd/night/stream.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"context"
+	"net"
+	"net/url"
+
+	"github.com/gernest/prom/api"
+	"github.com/gorilla/websocket"
+)
+
+// Opens a websocket connection using ws as url and reads the received messages
+// as json of type *api.TestSuite.
+//
+// If handler is not nil, for every successful read the handler will be invoked
+// passing the decoded *api.TestSuite as argument.
+func streamResponse(ctx context.Context, ws string, handler func(*api.TestSuite)) error {
+	u, err := url.Parse(ws)
+	if err != nil {
+		return err
+	}
+	u.Scheme = "tcp"
+	d, err := net.Dial("tcp", u.Host)
+	if err != nil {
+		return err
+	}
+	conn, _, err := websocket.NewClient(d, u, nil, 1024, 1024)
+	if err != nil {
+		return err
+	}
+	for {
+		select {
+		case <-ctx.Done():
+			return conn.Close()
+		default:
+			ts := &api.TestSuite{}
+			err := conn.ReadJSON(ts)
+			if err != nil {
+				return err
+			}
+			if handler != nil {
+				handler(ts)
+			}
+		}
+	}
+}

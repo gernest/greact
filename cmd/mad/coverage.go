6d5cced5ed6803d844900533893e124d7a7be8a4
gernest <geofreyernest@live.com>
2018-05-28 22:07:45 +0300

    clear chrome based coverage
    This is unreliable and brings more problems

--- cmd/mad/coverage.go
+++ cmd/mad/coverage.go
@@ -1,30 +1,9 @@
 package main
 
 import (
-	"encoding/json"
-	"io/ioutil"
-	"path/filepath"
-
-	"github.com/gernest/mad/config"
-	"github.com/gernest/mad/cover"
-	"github.com/mafredri/cdp/protocol/profiler"
 	"github.com/urfave/cli"
 )
 
 func runCoverage(ctx *cli.Context) error {
-	cfg, err := config.Load(ctx)
-	if err != nil {
-		return err
-	}
-	coverageFile := filepath.Join(cfg.OutputPath, cfg.Coverfile)
-	b, err := ioutil.ReadFile(coverageFile)
-	if err != nil {
-		return err
-	}
-	p := []profiler.Profile{}
-	err = json.Unmarshal(b, &p)
-	if err != nil {
-		return err
-	}
-	return cover.Process(cfg, p)
+	return nil
 }

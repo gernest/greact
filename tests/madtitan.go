1f15349f85378f7bfe179adadb2478434e46316b
gernest <geofreyernest@live.com>
2018-06-04 02:08:07 +0300

    add test for Render func

--- tests/madtitan.go
+++ tests/madtitan.go
@@ -67,3 +67,23 @@ func TestRenderBody() mad.Integration {
 		}),
 	)
 }
+
+func TestRender() mad.Integration {
+	txt := "hello,world"
+	return mad.Render("Render",
+		func() interface{} {
+			return vecty.Text(txt)
+		},
+		mad.It("must render the node in the html body", func(t mad.T) {
+			defer func() {
+				if err := recover(); err != nil {
+					t.Error(err)
+				}
+			}()
+			o := js.Global.Get("document").Get("body").Get("textContent").String()
+			if o != txt {
+				t.Errorf("expected %s got %s", txt, o)
+			}
+		}),
+	)
+}

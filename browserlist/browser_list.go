959540ed47a726c97e4d472b8ef74c0d1149ff5f
gernest <geofreyernest@live.com>
2018-06-07 16:19:42 +0300

    define version struct

--- browserlist/browser_list.go
+++ browserlist/browser_list.go
@@ -1,8 +1,11 @@
 package browserlist
 
-import "strings"
+import (
+	"strconv"
+	"strings"
+)
 
-type filter func(name, version string) bool
+type filter func(name string, version version) bool
 
 func query(str string) filter {
 	str = strings.TrimSpace(str)
@@ -12,8 +15,8 @@ func query(str string) filter {
 		ver := strings.TrimSpace(parts[1])
 		if len(parts[0]) == 2 {
 			if parts[0][1] == '=' {
-				return func(name, version string) bool {
-					return version <= ver
+				return func(name string, v version) bool {
+					return v.le(ver)
 				}
 			}
 			return noop
@@ -23,8 +26,8 @@ func query(str string) filter {
 		ver := strings.TrimSpace(parts[1])
 		if len(parts[0]) == 2 {
 			if parts[0][1] == '=' {
-				return func(name, version string) bool {
-					return version >= ver
+				return func(name string, v version) bool {
+					return v.ge(ver)
 				}
 			}
 			return noop
@@ -35,7 +38,7 @@ func query(str string) filter {
 	case "cover":
 	default:
 		if n, ok := aliasReverse[parts[0]]; ok {
-			return func(name, version string) bool {
+			return func(name string, v version) bool {
 				return name == n
 			}
 		}
@@ -43,7 +46,7 @@ func query(str string) filter {
 	return noop
 }
 
-func noop(_, _ string) bool {
+func noop(_ string, _ version) bool {
 	return false
 }
 
@@ -78,7 +81,64 @@ func init() {
 }
 
 func not(f filter) filter {
-	return func(name, version string) bool {
+	return func(name string, version version) bool {
 		return !f(name, version)
 	}
+}
+
+type version string
+
+func (v version) eq(v2 string) bool {
+	return string(v) == v2
+}
+
+func (v version) gt(v2 string) bool {
+	return v.filter(v2, func(a, b int) bool {
+		return a > b
+	})
+}
+
+func (v version) lt(v2 string) bool {
+	return v.filter(v2, func(a, b int) bool {
+		return a < b
+	})
+}
+
+func (v version) ge(v2 string) bool {
+	return v.filter(v2, func(a, b int) bool {
+		return a >= b
+	})
+}
+
+func (v version) le(v2 string) bool {
+	return v.filter(v2, func(a, b int) bool {
+		return a <= b
+	})
+}
+
+func (v version) filter(v2 string, fn func(a, b int) bool) bool {
+	s := string(v)
+	if s == "" {
+		return false
+	}
+	p1 := strings.Split(s, ".")
+	p2 := strings.Split(v2, ".")
+	n := len(p1)
+	if len(p2) < n {
+		n = len(p2)
+	}
+	for i := 0; i < n; i++ {
+		a, err := strconv.Atoi(p1[i])
+		if err != nil {
+			panic(err)
+		}
+		b, err := strconv.Atoi(p2[i])
+		if err != nil {
+			panic(err)
+		}
+		if fn(a, b) {
+			return true
+		}
+	}
+	return false
 }

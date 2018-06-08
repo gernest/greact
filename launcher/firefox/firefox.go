253ea427b1e04b10f8832500313fb483009d65d3
gernest <geofreyernest@live.com>
2018-06-08 23:01:26 +0300

    add launcher

--- launcher/firefox/firefox.go
+++ launcher/firefox/firefox.go
@@ -1,14 +1,24 @@
 package firefox
 
 import (
+	"context"
+	"errors"
 	"fmt"
+	"io/ioutil"
+	"net"
 	"os"
+	"os/exec"
 	"path/filepath"
 	"regexp"
 	"runtime"
 	"sort"
 	"strings"
+	"time"
 )
+
+var errFirefoxNotInstalled = errors.New("no Firefox installations found")
+
+const DefaultPort = 6000
 
 // Pref returns default firefox preferences.
 func Pref() string {
@@ -171,5 +181,130 @@ func Find(typ Type) string {
 		return firefoxNightly()
 	default:
 		return ""
+	}
+}
+
+type Options struct {
+	StartingURL        string
+	FirefoxFlags       []string
+	Type               Type
+	Port               int
+	FirefoxPath        string
+	IgnoreDefaultFlags bool
+	UserDataDir        string
+
+	//The time taken to wait for chrome to be ready.
+	WaitTimeout time.Duration
+
+	ProfilePath string
+	Verbose     bool
+}
+
+func (o Options) Flags() []string {
+	var f []string
+	if !o.IgnoreDefaultFlags {
+		f = defaultFlags()
+	}
+	f = append(f, "-start-debugger-server="+fmt.Sprint(o.Port),
+		"-profile="+o.ProfilePath,
+	)
+	f = append(f, o.FirefoxFlags...)
+	f = append(f, o.StartingURL)
+	return f
+}
+
+func defaultFlags() []string {
+	return []string{}
+}
+
+type Launcher struct {
+	Opts   Options
+	Cmd    *exec.Cmd
+	cancel func()
+}
+
+func New(opts Options) (*Launcher, error) {
+	if opts.StartingURL == "" {
+		opts.StartingURL = "about:blank"
+	}
+	if opts.FirefoxFlags == nil {
+		opts.FirefoxFlags = []string{}
+	}
+	if opts.UserDataDir == "" {
+		dir := filepath.Join(os.TempDir(), "mad-launcher")
+		err := os.MkdirAll(dir, 0755)
+		if err != nil {
+			return nil, err
+		}
+		p := filepath.Join(dir, "firefox_profile.js")
+		err = ioutil.WriteFile(p, []byte(Pref()), 0600)
+		if err != nil {
+			return nil, err
+		}
+		opts.ProfilePath = p
+		opts.UserDataDir = dir
+	}
+	if opts.FirefoxPath == "" {
+		i := Find(opts.Type)
+		if i == "" {
+			return nil, errFirefoxNotInstalled
+		}
+		opts.FirefoxPath = i
+	}
+	if opts.Port == 0 {
+		opts.Port = DefaultPort
+	}
+	if opts.WaitTimeout == 0 {
+		opts.WaitTimeout = 30 * time.Second
+	}
+	return &Launcher{Opts: opts}, nil
+}
+
+func (l *Launcher) Run(bctx context.Context) error {
+	ctx, cancel := context.WithCancel(bctx)
+	l.Cmd = exec.CommandContext(ctx, l.Opts.FirefoxPath, l.Opts.Flags()...)
+	l.Cmd.Stdout = os.Stdout
+	l.Cmd.Stderr = os.Stdout
+	fmt.Println(l.Cmd.Args)
+	l.cancel = cancel
+	return l.Cmd.Run()
+}
+
+func (l *Launcher) Stop() error {
+	l.cancel()
+	return nil
+}
+
+func (l *Launcher) Ready() error {
+	if l.Opts.Verbose {
+		fmt.Print("Waiting for firefox ...")
+	}
+	status := "."
+	tick := time.NewTicker(time.Second / 3)
+	defer tick.Stop()
+	o := time.NewTimer(l.Opts.WaitTimeout)
+	defer o.Stop()
+	for {
+		select {
+		case <-o.C:
+			if l.Opts.Verbose {
+				fmt.Println(".")
+			}
+			return errors.New("timeout waiting for firefox to be ready")
+		case <-tick.C:
+			conn, err := net.Dial("tcp", fmt.Sprintf(":%d", l.Opts.Port))
+			if err != nil {
+				if l.Opts.Verbose {
+					fmt.Print(status)
+				}
+				status += "."
+				continue
+			}
+			conn.Close()
+			if l.Opts.Verbose {
+				fmt.Println("done")
+			}
+			return nil
+		}
 	}
 }
--- launcher/firefox/firefox_test.go
+++ launcher/firefox/firefox_test.go
@@ -1,10 +1,32 @@
 package firefox
 
-import "testing"
+import (
+	"context"
+	"testing"
+)
 
 func TestGet(t *testing.T) {
-	v := Find(Firefox)
-	if v == "" {
-		t.Fatal("expected firefox")
+	l, err := New(Options{
+		Type:         Firefox,
+		Verbose:      true,
+		FirefoxFlags: []string{"-headless"}})
+	if err != nil {
+		t.Fatal(err)
+	}
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+	go func() {
+		err = l.Run(ctx)
+		if err != nil {
+			t.Fatal(err)
+		}
+	}()
+	err = l.Ready()
+	if err != nil {
+		t.Fatal(err)
+	}
+	err = l.Stop()
+	if err != nil {
+		t.Fatal(err)
 	}
 }

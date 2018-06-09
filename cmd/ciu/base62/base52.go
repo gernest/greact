427bae0a3fc2089d707f02993195aaa60e276406
gernest <geofreyernest@live.com>
2018-06-09 14:06:59 +0300

    add baseg2 encode/decode functions from caniuse-lite

---
+++ cmd/ciu/base62/base52.go
@@ -0,0 +1,29 @@
+package base62
+
+import (
+	"math"
+	"strings"
+)
+
+const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
+
+func Encode(i int64) (v string) {
+	r := i
+	for r != 0 {
+		v += string(characters[r%62])
+		r = r / 62
+	}
+	return
+}
+
+func Decode(src string) (r int64) {
+	if src == "" {
+		return 0
+	}
+	for k, v := range src {
+		i := strings.Index(characters, string(v))
+		n := math.Pow(62, float64(k))
+		r += int64(i) * int64(n)
+	}
+	return
+}

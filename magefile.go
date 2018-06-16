1ecc916403d7fa50e09b2ad954e1640a692a15c2
gernest <geofreyernest@live.com>
2018-06-16 11:49:14 +0300

    add mage test  target

--- magefile.go
+++ magefile.go
@@ -6,20 +6,7 @@ import (
 	"github.com/magefile/mage/sh"
 )
 
-const pkg = "github.com/gernest/vected"
-
-func Grid() error {
-	return sh.RunV("gopherjs", "build", "-o", "grid/demo/main.js", pkg+"/grid/demo/")
-}
-
-func Icon() error {
-	return sh.RunV("gopherjs", "build", "-o", "icon/demo/main.js", pkg+"/icon/demo/")
-}
-
-func Divider() error {
-	return sh.RunV("gopherjs", "build", "-o", "divider/demo/main.js", pkg+"/divider/demo/")
-}
-
-func Events() error {
-	return sh.RunV("gopherjs", "build", "-o", "event/demo/main.js", pkg+"/event/demo/")
+// Test executes mad test command.
+func Test() error {
+	return sh.RunV("mad", "test")
 }

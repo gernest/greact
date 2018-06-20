2827d18514844733cda51a3e029f93ca3e4b7f98
gernest <geofreyernest@live.com>
2018-06-20 23:49:50 +0300

    add style/motion/slide

---
+++ web/style/motion/slide/slide.go
@@ -0,0 +1,131 @@
+package slide
+
+import (
+	"github.com/gernest/gs"
+	"github.com/gernest/vected/web/mixins"
+	"github.com/gernest/vected/web/themes"
+)
+
+//keyframes names
+const (
+	Up    = "slideUp"
+	Down  = "slideDown"
+	Left  = "slideLeft"
+	Right = "slideRight"
+)
+
+func KeyFrames() gs.CSSRule {
+	return gs.CSS(
+		gs.KeyFrame(Up+"In",
+			gs.S("0%",
+				gs.P("opacity", "0"),
+				gs.P("transform-origin", "0% 0%"),
+				gs.P("transform", "scaleY(.8)"),
+			),
+			gs.S("100%",
+				gs.P("opacity", "1"),
+				gs.P("transform-origin", "0% 0%"),
+				gs.P("transform", "scaleY(1)"),
+			),
+		),
+		gs.KeyFrame(Up+"Out",
+			gs.S("0%",
+				gs.P("opacity", "1"),
+				gs.P("transform-origin", "0% 0%"),
+				gs.P("transform", "scaleY(1)"),
+			),
+			gs.S("100%",
+				gs.P("opacity", "0"),
+				gs.P("transform-origin", "0% 0%"),
+				gs.P("transform", "scaleY(.8)"),
+			),
+		),
+		gs.KeyFrame(Down+"In",
+			gs.S("0%",
+				gs.P("opacity", "0"),
+				gs.P("transform-origin", "100% 100%"),
+				gs.P("transform", "scaleY(.8)"),
+			),
+			gs.S("100%",
+				gs.P("opacity", "1"),
+				gs.P("transform-origin", "100% 100%"),
+				gs.P("transform", "scaleY(1)"),
+			),
+		),
+		gs.KeyFrame(Down+"Out",
+			gs.S("0%",
+				gs.P("opacity", "1"),
+				gs.P("transform-origin", "100% 100%"),
+				gs.P("transform", "scaleY(1)"),
+			),
+			gs.S("100%",
+				gs.P("opacity", "0"),
+				gs.P("transform-origin", "100% 100%"),
+				gs.P("transform", "scaleY(.8)"),
+			),
+		),
+		gs.KeyFrame(Left+"In",
+			gs.S("0%",
+				gs.P("opacity", "0"),
+				gs.P("transform-origin", "0% 0%"),
+				gs.P("transform", "scaleX(.8)"),
+			),
+			gs.S("100%",
+				gs.P("opacity", "1"),
+				gs.P("transform-origin", "0% 0%"),
+				gs.P("transform", "scaleX(1)"),
+			),
+		),
+		gs.KeyFrame(Left+"Out",
+			gs.S("0%",
+				gs.P("opacity", "1"),
+				gs.P("transform-origin", "0% 0%"),
+				gs.P("transform", "scaleX(1)"),
+			),
+			gs.S("100%",
+				gs.P("opacity", "0"),
+				gs.P("transform-origin", "0% 0%"),
+				gs.P("transform", "scaleX(.8)"),
+			),
+		),
+		gs.KeyFrame(Left+"In",
+			gs.S("0%",
+				gs.P("opacity", "0"),
+				gs.P("transform-origin", "100% 0%"),
+				gs.P("transform", "scaleX(.8)"),
+			),
+			gs.S("100%",
+				gs.P("opacity", "1"),
+				gs.P("transform-origin", "100% 0%"),
+				gs.P("transform", "scaleX(1)"),
+			),
+		),
+		gs.KeyFrame(Left+"Out",
+			gs.S("0%",
+				gs.P("opacity", "1"),
+				gs.P("transform-origin", "100% 0%"),
+				gs.P("transform", "scaleX(1)"),
+			),
+			gs.S("100%",
+				gs.P("opacity", "0"),
+				gs.P("transform-origin", "100% 0%"),
+				gs.P("transform", "scaleX(.8)"),
+			),
+		),
+	)
+}
+
+func Motion(klass, keyframe string) gs.CSSRule {
+	return gs.CSS(
+		mixins.MakeMotion(klass, keyframe, themes.Default.AnimationDurationBase),
+		gs.S(klass+"-enter",
+			gs.S("&,"+klass+"-appear",
+				gs.P("opacity", "0"),
+				gs.P("animation-timing-function", themes.Default.EaseOutQuint),
+			),
+		),
+		gs.S(klass+"-leave",
+			gs.P("animation-timing-function", themes.Default.EaseInQuint),
+		),
+	)
+}

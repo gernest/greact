7fcdc1ca0cdaa4cb7f0d97353f765216e2b1336e
gernest <geofreyernest@live.com>
2018-06-24 05:17:18 +0300

    start  experimenting on vected API

--- vected.go
+++ vected.go
@@ -1,1 +1,9 @@
 package vected
+
+import "io"
+
+// Component is an interface for reneding components. ctx is the data that will
+// be passed to the component as context.
+type Component interface {
+	Render(w io.Writer, ctx interface{}) error
+}

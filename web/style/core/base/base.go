ce6bb489a9799147643845aab499e6fee3eb9beb
gernest <geofreyernest@live.com>
2018-06-29 17:58:31 +0300

    add more base styles

--- web/style/core/base/base.go
+++ web/style/core/base/base.go
@@ -36,8 +36,36 @@ func Base() gs.CSSRule {
 			gs.P("unicode-range", "U+2018, U+2019, U+201c, U+201d"),
 		),
 		// HTML & Body reset
-		gs.S("html", gs.S("body"),
+		gs.S("html", gs.S("&,body"),
 			mixins.Square("100%"),
+		),
+		// remove the clear button of a text input control in IE10+
+		gs.S("input::-ms-clear", gs.S("&,input::-ms-reveal",
+			gs.P("display", "none"),
+		)),
+		// Document
+		//
+		// 1. Change from `box-sizing: content-box` so that `width` is not affected by `padding` or `border`.
+		// 2. Change the default font family in all browsers.
+		// 3. Correct the line height in all browsers.
+		// 4. Prevent adjustments of font size after orientation changes in IE on Windows Phone and in iOS.
+		// 5. Setting @viewport causes scrollbars to overlap content in IE11 and Edge, so
+		//    we force a non-overlapping, non-auto-hiding scrollbar to counteract.
+		// 6. Change the default tap highlight to be completely transparent in iOS.
+		gs.S("*", gs.S("&,*:::before", gs.S("&,*::after",
+			gs.P("box-sizing", "border-box"),
+		))),
+		gs.S("html",
+			gs.P("font-family", "sans-serif"),
+			gs.P("line-height", "1.15"),
+			gs.P("-webkit-text-size-adjust", "100%"),
+			gs.P("-ms-text-size-adjust", "100%"),
+			gs.P("-ms-overflow-style", "scrollbar"),
+			gs.P("-webkit-tap-highlight-color", "rgba(0, 0, 0, 0)"),
+		),
+		// IE10+ doesn't honor `<meta name="viewport">` in some cases.
+		gs.Cond("@-ms-viewport ",
+			gs.P("width", "device-width"),
 		),
 	)
 }

ea39f64d1cf6da2cd58e17806dba5fa09a3ae716
gernest <geofreyernest@live.com>
2018-07-02 00:18:02 +0300

    add props package

---
+++ props/props.go
@@ -0,0 +1,6 @@
+// Package props exposes structs and functions for manipulating of properties.
+// properties are values that can be passed around to components.
+package props
+
+// Props is a map of properties. These are used to pass values to components.
+type Props map[interface{}]interface{}

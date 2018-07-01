08f141d25da5abeee7d3abc77180507cb42e6458
gernest <geofreyernest@live.com>
2018-07-02 00:21:38 +0300

    add props.Merge

--- props/props.go
+++ props/props.go
@@ -4,3 +4,15 @@ package props
 
 // Props is a map of properties. These are used to pass values to components.
 type Props map[interface{}]interface{}
+
+// Merge returns new Props with values from b added to a.
+func Merge(a, b Props) Props {
+	m := make(Props)
+	for k, v := range a {
+		m[k] = v
+	}
+	for k, v := range b {
+		m[k] = v
+	}
+	return m
+}

2194a3abaed7b77dd450441751799f025280e599
gernest <geofreyernest@live.com>
2018-07-12 04:04:56 +0300

    move caniuse agents generation code to cmd/gen

--- cmd/gen/ciu/main.go
+++ cmd/gen/ciu/main.go
@@ -3,14 +3,70 @@ package main
 import (
 	"bytes"
 	"encoding/json"
+	"fmt"
 	"go/format"
 	"io"
 	"io/ioutil"
+	"os"
 	"sort"
-	"text/template"
 
+	"github.com/gernest/vected/lib/html/template"
 	"github.com/urfave/cli"
 )
+
+type Feature struct {
+	Stats map[string]map[string]string `json:"stats"`
+}
+
+type Agent struct {
+	Name                string
+	Browser             string             `json:"browser"`
+	Abbr                string             `json:"abbr"`
+	Prefix              string             `json:"prefix"`
+	Type                string             `json:"type"`
+	UsageGlobal         map[string]float64 `json:"usage_global"`
+	Versions            []string           `json:"versions"`
+	DataPrefixEceptions map[string]string  `json:"prefix_exceptions"`
+	VersionList         []Version          `json:"version_list"`
+}
+
+type Version struct {
+	Version     string `json:"version"`
+	ReleaseData int64  `json:"release_date"`
+}
+
+type Data struct {
+	Agents   map[string]Agent   `json:"agents"`
+	Features map[string]Feature `json:"data"`
+}
+
+func main() {
+	a := cli.NewApp()
+	a.Commands = []cli.Command{
+		{
+			Name:   "agents",
+			Action: AgentCMD,
+			Flags: []cli.Flag{
+				cli.StringFlag{
+					Name:  "full",
+					Value: "caniuse/fulldata-json//data-2.0.json",
+				},
+				cli.StringFlag{
+					Name:  "data",
+					Value: "caniuse/data.json",
+				},
+				cli.StringFlag{
+					Name:  "agents-file",
+					Value: "lib/ciu/agents/agents.go",
+				},
+			},
+		},
+	}
+	if err := a.Run(os.Args); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
 
 func invert(m map[string]string) map[string]string {
 	o := make(map[string]string)

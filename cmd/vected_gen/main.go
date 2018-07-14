959cd6a11b52fb0037e0e93cbcd993cec820bbcd
gernest <geofreyernest@live.com>
2018-07-14 23:55:19 +0300

    add includejs library and generation command

---
+++ cmd/vected_gen/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/gernest/vected/cmd/gen"
+	"github.com/urfave/cli"
+)
+
+func main() {
+	a := cli.NewApp()
+	a.Name = "vected_gen"
+	a.Usage = "provides various commands that generate code for vected project"
+	a.Commands = []cli.Command{
+		gen.Include(),
+	}
+	if err := a.Run(os.Args); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}

9eb6acb3ecb4967e7efe31ef78126869091d93fa
gernest <geofreyernest@live.com>
2018-07-19 23:18:24 +0300

    add go ebnf ror webidl

---
+++ lib/webidl/ebnf_test.go
@@ -0,0 +1,20 @@
+package webidl
+
+import (
+	"os"
+	"testing"
+
+	"golang.org/x/exp/ebnf"
+)
+
+func TestGrammar(t *testing.T) {
+	f, err := os.Open("grammar.ebnf")
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer f.Close()
+	_, err = ebnf.Parse("grammer", f)
+	if err != nil {
+		t.Fatal(err)
+	}
+}

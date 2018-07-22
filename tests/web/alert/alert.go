98bc583fb0b3ab12c6574307c013772df7e26668
gernest <geofreyernest@live.com>
2018-07-22 17:17:35 +0300

    move alert style to style package

--- tests/web/alert/alert.go
+++ tests/web/alert/alert.go
@@ -3,12 +3,12 @@ package alert
 import (
 	"github.com/gernest/vected/lib/gs"
 	"github.com/gernest/vected/lib/mad"
-	"github.com/gernest/vected/web/alert"
+	"github.com/gernest/vected/web/alert/style"
 )
 
 func TestStyle() mad.Test {
 	return mad.It("returns antd alert style", func(t mad.T) {
-		css := gs.ToString(alert.Style())
+		css := gs.ToString(style.Style())
 		if css != expect {
 			t.Error("got wrong styles")
 		}
--- web/alert/style/style.go
+++ web/alert/style/style.go
@@ -1,4 +1,4 @@
-package alert
+package style
 
 import (
 	"fmt"

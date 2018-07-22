1978ce7e7ba887a3603f6030c6c14588efb7b3b4
gernest <geofreyernest@live.com>
2018-07-22 17:21:38 +0300

    rename Style for affix to Affix

--- web/affix/style/style.go
+++ web/affix/style/style.go
@@ -7,8 +7,8 @@ import (
 
 var prefix = themes.Default.AntPrefix
 
-// Style returns css style for antd affix.
-func Style() gs.CSSRule {
+// Affix returns css style for antd affix.
+func Affix() gs.CSSRule {
 	return gs.S(prefix+"-affix",
 		gs.P("position", "fixed"),
 		gs.P("z-index", themes.Default.ZIndexAffix),

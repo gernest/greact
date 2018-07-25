eb37890e97b83f5facd7c579756f68c54864d7e5
gernest <geofreyernest@live.com>
2018-07-25 15:03:50 +0300

    generate compiled template

--- lib/xhtml/xhtml.go
+++ lib/xhtml/xhtml.go
@@ -1,86 +1,165 @@
 package xhtml
 
 import (
+	"bytes"
 	"fmt"
-	"io"
+	"go/ast"
+	"go/format"
+	"go/token"
 	"strings"
-	"sync"
+	"text/template"
 
-	"github.com/gernest/vected/lib/html/template"
 	"golang.org/x/net/html"
+	"golang.org/x/net/html/atom"
 )
 
-var globalCache = &NodeCache{nodes: make(map[string]*Node)}
-
-type NodeCache struct {
-	nodes map[string]*Node
-	mu    sync.RWMutex
-}
-
-func (n *NodeCache) Set(name string, v *Node) {
-	n.mu.Lock()
-	n.nodes[name] = v
-	n.mu.RUnlock()
-}
-
-func (n *NodeCache) Get(name string) (*Node, bool) {
-	n.mu.RLock()
-	v, ok := n.nodes[name]
-	n.mu.RUnlock()
-	return v, ok
-}
-
 type Node struct {
-	Tree          *html.Node
-	TemplateCache map[*html.Node]map[string]*template.Template
+	Type      html.NodeType
+	DataAtom  atom.Atom
+	Data      string
+	Namespace string
+	Attr      []html.Attribute
+	Children  []*Node
 }
 
-// Compile  parses src as html.
-func Compile(src io.Reader, name string) (*Node, error) {
-	name = strings.ToLower(name)
-	n, err := compile(src, name)
-	if err != nil {
-		return nil, err
+func Clone(n *html.Node, e *Node) {
+	for c := n.FirstChild; c != nil; c = c.NextSibling {
+		ch := &Node{
+			Type:      c.Type,
+			DataAtom:  c.DataAtom,
+			Data:      c.Data,
+			Namespace: c.Namespace,
+			Attr:      make([]html.Attribute, len(c.Attr)),
+		}
+		copy(ch.Attr, c.Attr)
+		e.Children = append(e.Children, ch)
+		Clone(c, ch)
 	}
-	globalCache.Set(name, n)
-	return n, nil
 }
 
-func compile(src io.Reader, name string) (*Node, error) {
-	doc, err := html.Parse(src)
-	if err != nil {
-		return nil, err
+func makeNode(n *Node) *ast.UnaryExpr {
+	return &ast.UnaryExpr{
+		Op: token.AND,
+		X: &ast.CompositeLit{
+			Type: &ast.SelectorExpr{
+				X:   &ast.Ident{Name: "xhtml"},
+				Sel: &ast.Ident{Name: "Node"},
+			},
+			Elts: []ast.Expr{
+				&ast.KeyValueExpr{
+					Key: &ast.Ident{Name: "Type"},
+					Value: &ast.BasicLit{
+						Kind:  token.INT,
+						Value: fmt.Sprint(n.Type),
+					},
+				},
+				makeDataAtomField(n.DataAtom),
+				&ast.KeyValueExpr{
+					Key: &ast.Ident{Name: "Data"},
+					Value: &ast.BasicLit{
+						Kind:  token.STRING,
+						Value: fmt.Sprintf("%q", n.Data),
+					},
+				},
+				&ast.KeyValueExpr{
+					Key: &ast.Ident{Name: "Namespace"},
+					Value: &ast.BasicLit{
+						Kind:  token.STRING,
+						Value: fmt.Sprintf("%q", n.Namespace),
+					},
+				},
+				&ast.KeyValueExpr{
+					Key:   &ast.Ident{Name: "Children"},
+					Value: makeChildren(n.Children),
+				},
+			},
+		},
 	}
-	idx := 0
-	cache := make(map[*html.Node]map[string]*template.Template)
-	var f func(*html.Node) error
-	f = func(n *html.Node) error {
-		m := make(map[string]*template.Template)
-		switch n.Type {
-		case html.TextNode:
-			if strings.Contains(n.Data, "{") {
-				tplName := fmt.Sprintf("__%s__%d__", name, idx)
-				t, err := template.New(tplName).Delims("{", "}").Parse(n.Data)
-				if err != nil {
-					return err
-				}
-				m[name] = t
-				n.Data = name
-			}
 
-		}
-		cache[n] = m
-		for c := n.FirstChild; c != nil; c = c.NextSibling {
-			err := f(c)
-			if err != nil {
-				return err
-			}
-		}
-		return nil
+}
+
+func makeDataAtomField(a atom.Atom) *ast.KeyValueExpr {
+	v := a.String()
+	if v == "" {
+		v = "Atom(0)"
+	} else {
+		v = strings.Title(v)
 	}
-	err = f(doc)
+	return &ast.KeyValueExpr{
+		Key: &ast.Ident{Name: "DataAtom"},
+		Value: &ast.SelectorExpr{
+			X:   &ast.Ident{Name: "atom"},
+			Sel: &ast.Ident{Name: v},
+		},
+	}
+}
+
+func makeChildren(nodes []*Node) *ast.CompositeLit {
+	return &ast.CompositeLit{
+		Type: &ast.ArrayType{
+			Elt: &ast.StarExpr{
+				X: &ast.SelectorExpr{
+					X:   &ast.Ident{Name: "xhtml"},
+					Sel: &ast.Ident{Name: "Node"},
+				},
+			},
+		},
+		Elts: makeArray(nodes),
+	}
+}
+
+func makeArray(nodes []*Node) []ast.Expr {
+	if nodes != nil {
+		var ls []ast.Expr
+		for _, v := range mkArrayElems(nodes) {
+			ls = append(ls, v)
+		}
+		return ls
+	}
+	return []ast.Expr{}
+}
+
+func mkArrayElems(n []*Node) []*ast.UnaryExpr {
+	var ls []*ast.UnaryExpr
+	for _, v := range n {
+		ls = append(ls, makeNode(v))
+	}
+	return ls
+}
+
+const fnTpl = `package {{.package}}
+
+import (
+	"github.com/gernest/vected/lib/xhtml"
+	"golang.org/x/net/html/atom"
+)
+
+func ({{.recv}} {{.struct}})Render()*xhtml.Node{
+	return {{.node}}
+}
+`
+
+var tpl = template.Must(template.New("n").Parse(fnTpl))
+
+func generatePackage(n *Node) ([]byte, error) {
+	var buf bytes.Buffer
+	node := makeNode(n)
+	fset := token.NewFileSet()
+	err := format.Node(&buf, fset, node)
 	if err != nil {
 		return nil, err
 	}
-	return &Node{Tree: doc, TemplateCache: cache}, nil
+	nstr := buf.String()
+	buf.Reset()
+	err = tpl.Execute(&buf, map[string]interface{}{
+		"package": "test",
+		"recv":    "ts",
+		"struct":  "Test",
+		"node":    nstr,
+	})
+	if err != nil {
+		return nil, err
+	}
+	// return format.Source(buf.Bytes())
+	return buf.Bytes(), nil
 }
--- lib/xhtml/xhtml_test.go
+++ lib/xhtml/xhtml_test.go
@@ -1,1 +1,150 @@
 package xhtml
+
+import (
+	"bytes"
+	"go/ast"
+	"go/parser"
+	"go/token"
+	"io/ioutil"
+	"strings"
+	"testing"
+
+	"golang.org/x/net/html"
+)
+
+func TestHTML(t *testing.T) {
+	src := `<div> hello, world</div>`
+	doc, err := html.Parse(strings.NewReader(src))
+	if err != nil {
+		t.Fatal(err)
+	}
+	o := &Node{}
+	Clone(doc, o)
+
+	t.Error(o.Format(0))
+	// t.Error(pretty.Sprint(o))
+}
+
+const helloSrc = `package test
+
+func run()*xhtml.Node{
+	return &xhtml.Node{
+		Type:      0x0,
+		DataAtom:  atom.Div,
+		Data:      "",
+		Namespace: "",
+		Children: []*xhtml.Node {
+			&xhtml.Node{
+				Type:      0x3,
+				DataAtom:  0x14704,
+				Data:      "html",
+				Namespace: "",
+				Attr:[]html.Attribute{
+				},
+				// Children: {
+				// 	&xhtml.Node{
+				// 		Type:      0x3,
+				// 		DataAtom:  0x32904,
+				// 		Data:      "head",
+				// 		Namespace: "",
+				// 		Children: nil,
+				// 	},
+				// 	&xhtml.Node{
+				// 		Type:      0x3,
+				// 		DataAtom:  0x2804,
+				// 		Data:      "body",
+				// 		Namespace: "",
+				// 		Children: []xhtml.Node{
+				// 			&xhtml.Node{
+				// 				Type:      0x3,
+				// 				DataAtom:  0x16b03,
+				// 				Data:      "div",
+				// 				Namespace: "",
+				// 				Children: []xhtml.Node{
+				// 					&xhtml.Node{
+				// 						Type:      0x1,
+				// 						DataAtom:  0x0,
+				// 						Data:      " hello, world",
+				// 						Namespace: "",
+				// 					},
+				// 				},
+				// 			},
+				// 		},
+				// 	},
+				// },
+			},
+			&xhtml.Node{
+				Type:      0x3,
+				DataAtom:  0x14704,
+				Data:      "html",
+				Namespace: "",
+				Attr:[]html.Attribute{
+				},
+				// Children: {
+				// 	&xhtml.Node{
+				// 		Type:      0x3,
+				// 		DataAtom:  0x32904,
+				// 		Data:      "head",
+				// 		Namespace: "",
+				// 		Children: nil,
+				// 	},
+				// 	&xhtml.Node{
+				// 		Type:      0x3,
+				// 		DataAtom:  0x2804,
+				// 		Data:      "body",
+				// 		Namespace: "",
+				// 		Children: []xhtml.Node{
+				// 			&xhtml.Node{
+				// 				Type:      0x3,
+				// 				DataAtom:  0x16b03,
+				// 				Data:      "div",
+				// 				Namespace: "",
+				// 				Children: []xhtml.Node{
+				// 					&xhtml.Node{
+				// 						Type:      0x1,
+				// 						DataAtom:  0x0,
+				// 						Data:      " hello, world",
+				// 						Namespace: "",
+				// 					},
+				// 				},
+				// 			},
+				// 		},
+				// 	},
+				// },
+			},
+		},
+	}
+}
+`
+
+func TestAST(t *testing.T) {
+	fset := token.NewFileSet() // positions are relative to fset
+	f, err := parser.ParseFile(fset, "", helloSrc, 0)
+	if err != nil {
+		panic(err)
+	}
+
+	// Print the AST.
+	ast.Print(fset, f)
+	var buf bytes.Buffer
+	ast.Fprint(&buf, fset, f, ast.NotNilFilter)
+	ioutil.WriteFile("ast.txt", buf.Bytes(), 0600)
+}
+
+func TestMkaeNode(t *testing.T) {
+	src := `<div> hello, world</div>`
+	doc, err := html.Parse(strings.NewReader(src))
+	if err != nil {
+		t.Fatal(err)
+	}
+	o := &Node{
+		DataAtom: doc.DataAtom,
+		Data:     doc.Data,
+	}
+	Clone(doc, o)
+	v, err := generatePackage(o)
+	if err != nil {
+		t.Fatal(err)
+	}
+	t.Error(string(v))
+}

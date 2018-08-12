b772e0a63607331028d6b954fea0a15d18bab88d
gernest <geofreyernest@live.com>
2018-08-12 11:18:40 +0300

    add demo  was dom callbacks

--- vdom/dom/demo/main.go
+++ vdom/dom/demo/main.go
@@ -1,9 +1,23 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"syscall/js"
 )
 
+var doc = js.Global().Get("document")
+
 func main() {
-	fmt.Println("hello,world")
+	ctx, _ := context.WithCancel(context.TODO())
+	root := doc.Call("getElementById", "main")
+	btn := doc.Call("createElement", "button")
+	txt := doc.Call("createTextNode", "Click me")
+	btn.Call("appendChild", txt)
+	cb := js.NewCallback(func(args []js.Value) {
+		fmt.Println("button clicked")
+	})
+	btn.Call("addEventListener", "click", cb)
+	root.Call("appendChild", btn)
+	<-ctx.Done()
 }

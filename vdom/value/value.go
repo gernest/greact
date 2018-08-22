c56d38125294527f286eb26d5f2995335d8c0d1b
gernest <geofreyernest@live.com>
2018-08-22 13:23:54 +0300

    add Keys

--- vdom/value/value.go
+++ vdom/value/value.go
@@ -29,3 +29,17 @@ type Value interface {
 type Callback interface {
 	Release()
 }
+
+// Keys is like Object.keys, this returns nil if v is not an object.
+func Keys(v Value) []string {
+	if v.Type() != TypeObject {
+		return nil
+	}
+	k := v.Call("keys")
+	var o []string
+	size := k.Get("length").Int()
+	for i := 0; i < size; i++ {
+		o = append(o, k.Index(i).String())
+	}
+	return o
+}

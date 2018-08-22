c8eb66895c2a67694e6538177ef0b710d981cf95
gernest <geofreyernest@live.com>
2018-08-22 17:02:11 +0300

    use reflection for Constructor when it is missing

--- component.go
+++ component.go
@@ -3,6 +3,7 @@ package vected
 import (
 	"container/list"
 	"context"
+	"reflect"
 
 	"github.com/gernest/vected/prop"
 )
@@ -13,6 +14,13 @@ func createComponent(ctx context.Context, cmp Component, props prop.Props) Compo
 	var ncmp Component
 	if in, ok := cmp.(Constructor); ok {
 		ncmp = in.New()
+	} else {
+		// we use reflection to create a new component
+		v := reflect.ValueOf(cmp)
+		if v.Kind() != reflect.Ptr || v.Elem().Kind() != reflect.Struct {
+			panic("component must be pointer to struct")
+		}
+		ncmp = reflect.New(v.Elem().Type()).Interface().(Component)
 	}
 	core := ncmp.core()
 	core.context = ctx
--- component_test.go
+++ component_test.go
@@ -9,6 +9,10 @@ import (
 	"github.com/gernest/vected/vdom"
 )
 
+var H = vdom.New
+var HA = vdom.Attr
+var HAT = vdom.Attrs
+
 type Hello struct {
 	Core
 }
@@ -17,12 +21,18 @@ func (*Hello) New() Component {
 	return &Hello{}
 }
 
-var H = vdom.New
-var HA = vdom.Attr
-var HAT = vdom.Attrs
-
 // Render implements vected.Renderer interface.
 func (h *Hello) Render(ctx context.Context, props prop.Props, state state.State) *vdom.Node {
+	return H(3, "", "div", nil,
+		H(1, "", "hello,world", nil))
+}
+
+type HelloPlain struct {
+	Core
+}
+
+// Render implements vected.Renderer interface.
+func (h HelloPlain) Render(ctx context.Context, props prop.Props, state state.State) *vdom.Node {
 	return H(3, "", "div", nil,
 		H(1, "", "hello,world", nil))
 }
@@ -32,9 +42,24 @@ func TestCreateComponent(t *testing.T) {
 		n := createComponent(context.Background(), &Hello{}, make(prop.Props))
 		core := n.core()
 		if core.context == nil {
-			t.Error("expected context to be set")
+			ts.Error("expected context to be set")
 		}
 		if core.props == nil {
+			ts.Error("expected initial props to be set")
+		}
+	})
+	t.Run("must initialize a new instance without Constructor", func(ts *testing.T) {
+		n := createComponent(context.Background(), &HelloPlain{}, prop.Props{
+			"key": "value",
+		})
+		core := n.core()
+		if core.context == nil {
+			ts.Error("expected context to be set")
+		}
+		if core.props == nil {
+			ts.Error("expected initial props to be set")
+		}
+		if core.props.String("key").Value != "value" {
 			t.Error("expected initial props to be set")
 		}
 	})

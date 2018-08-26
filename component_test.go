68ecc944cf7b62129f3667ead9f727f5fb096fe9
gernest <geofreyernest@live.com>
2018-08-26 22:14:52 +0300

    cleanup

--- component_test.go
+++ component_test.go
@@ -1,66 +1,57 @@
 package vected
 
-import (
-	"context"
-	"testing"
+// var H = vdom.New
+// var HA = vdom.Attr
+// var HAT = vdom.Attrs
 
-	"github.com/gernest/vected/prop"
-	"github.com/gernest/vected/state"
-	"github.com/gernest/vected/vdom"
-)
+// type Hello struct {
+// 	Core
+// }
 
-var H = vdom.New
-var HA = vdom.Attr
-var HAT = vdom.Attrs
+// func (*Hello) New() Component {
+// 	return &Hello{}
+// }
 
-type Hello struct {
-	Core
-}
+// // Render implements vected.Renderer interface.
+// func (h *Hello) Render(ctx context.Context, props prop.Props, state state.State) *vdom.Node {
+// 	return H(3, "", "div", nil,
+// 		H(1, "", "hello,world", nil))
+// }
 
-func (*Hello) New() Component {
-	return &Hello{}
-}
+// type HelloPlain struct {
+// 	Core
+// }
 
-// Render implements vected.Renderer interface.
-func (h *Hello) Render(ctx context.Context, props prop.Props, state state.State) *vdom.Node {
-	return H(3, "", "div", nil,
-		H(1, "", "hello,world", nil))
-}
+// // Render implements vected.Renderer interface.
+// func (h HelloPlain) Render(ctx context.Context, props prop.Props, state state.State) *vdom.Node {
+// 	return H(3, "", "div", nil,
+// 		H(1, "", "hello,world", nil))
+// }
 
-type HelloPlain struct {
-	Core
-}
-
-// Render implements vected.Renderer interface.
-func (h HelloPlain) Render(ctx context.Context, props prop.Props, state state.State) *vdom.Node {
-	return H(3, "", "div", nil,
-		H(1, "", "hello,world", nil))
-}
-
-func TestCreateComponent(t *testing.T) {
-	t.Run("must set initial context and props", func(ts *testing.T) {
-		n := createComponent(context.Background(), &Hello{}, make(prop.Props))
-		core := n.core()
-		if core.context == nil {
-			ts.Error("expected context to be set")
-		}
-		if core.props == nil {
-			ts.Error("expected initial props to be set")
-		}
-	})
-	t.Run("must initialize a new instance without Constructor", func(ts *testing.T) {
-		n := createComponent(context.Background(), &HelloPlain{}, prop.Props{
-			"key": "value",
-		})
-		core := n.core()
-		if core.context == nil {
-			ts.Error("expected context to be set")
-		}
-		if core.props == nil {
-			ts.Error("expected initial props to be set")
-		}
-		if core.props.String("key").Value != "value" {
-			t.Error("expected initial props to be set")
-		}
-	})
-}
+// func TestCreateComponent(t *testing.T) {
+// 	t.Run("must set initial context and props", func(ts *testing.T) {
+// 		n := createComponent(context.Background(), &Hello{}, make(prop.Props))
+// 		core := n.core()
+// 		if core.context == nil {
+// 			ts.Error("expected context to be set")
+// 		}
+// 		if core.props == nil {
+// 			ts.Error("expected initial props to be set")
+// 		}
+// 	})
+// 	t.Run("must initialize a new instance without Constructor", func(ts *testing.T) {
+// 		n := createComponent(context.Background(), &HelloPlain{}, prop.Props{
+// 			"key": "value",
+// 		})
+// 		core := n.core()
+// 		if core.context == nil {
+// 			ts.Error("expected context to be set")
+// 		}
+// 		if core.props == nil {
+// 			ts.Error("expected initial props to be set")
+// 		}
+// 		if core.props.String("key").Value != "value" {
+// 			t.Error("expected initial props to be set")
+// 		}
+// 	})
+// }
--- vected_test.go
+++ vected_test.go
@@ -1,45 +1,35 @@
 package vected
 
-import (
-	"context"
-	"testing"
+// var _ Component = (*A)(nil)
 
-	"github.com/gernest/vected/prop"
-	"github.com/gernest/vected/state"
-	"github.com/gernest/vected/vdom"
-	"github.com/gernest/vected/vdom/testutil"
-)
+// type A struct {
+// 	Core
+// 	cb func()
+// }
 
-var _ Component = (*A)(nil)
+// func (*A) Template() string {
+// 	return ``
+// }
+// func (a *A) Render(context.Context, prop.Props, state.State) *vdom.Node {
+// 	return nil
+// }
+// func (a *A) ComponentDidMount() {
+// 	if a.cb != nil {
+// 		a.cb()
+// 	}
+// }
 
-type A struct {
-	Core
-	cb func()
-}
-
-func (*A) Template() string {
-	return ``
-}
-func (a *A) Render(context.Context, prop.Props, state.State) *vdom.Node {
-	return nil
-}
-func (a *A) ComponentDidMount() {
-	if a.cb != nil {
-		a.cb()
-	}
-}
-
-func TestDOM(t *testing.T) {
-	t.Run("removeChildren", func(ts *testing.T) {
-		e := testutil.NewObject()
-		for i := 0; i < 5; i++ {
-			e.Call("appendChild", testutil.NewObject())
-		}
-		removeChildren(e)
-		ch := e.Get("childNodes").Get("length").Int()
-		expect := 0
-		if ch != expect {
-			ts.Errorf("expected %d got %d", expect, ch)
-		}
-	})
-}
+// func TestDOM(t *testing.T) {
+// 	t.Run("removeChildren", func(ts *testing.T) {
+// 		e := testutil.NewObject()
+// 		for i := 0; i < 5; i++ {
+// 			e.Call("appendChild", testutil.NewObject())
+// 		}
+// 		removeChildren(e)
+// 		ch := e.Get("childNodes").Get("length").Int()
+// 		expect := 0
+// 		if ch != expect {
+// 			ts.Errorf("expected %d got %d", expect, ch)
+// 		}
+// 	})
+// }

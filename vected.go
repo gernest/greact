be07ee8d55d05305b337052620c378062c24123a
gernest <geofreyernest@live.com>
2018-09-19 05:31:30 +0300

    move things around

--- vected.go
+++ vected.go
@@ -28,6 +28,7 @@ const (
 
 // AttrKey is a key used to store node's attributes/props
 const AttrKey = "__vected_attr__"
+const svg = "http://www.w3.org/2000/svg'"
 
 // This tracks the last id issued. We use sync pool to reuse component id's.
 //
@@ -69,6 +70,82 @@ var idPool = &sync.Pool{
 type Component interface {
 	Render(context.Context, Props, State) *Node
 	core() *Core
+}
+
+type Type int
+
+const (
+	TypeUndefined Type = iota
+	TypeNull
+	TypeBoolean
+	TypeNumber
+	TypeString
+	TypeSymbol
+	TypeObject
+	TypeFunction
+)
+
+type Value interface {
+	Bool() bool
+	Call(m string, args ...interface{}) Value
+	Float() float64
+	Get(string) Value
+	Index(int) Value
+	Int() int
+	Invoke(args ...interface{}) Value
+	Set(p string, x interface{})
+	String() string
+	Type() Type
+}
+
+type Callback interface {
+	Release()
+}
+
+// Keys is like Object.keys, this returns nil if v is not an object.
+func Keys(v Value) []string {
+	if v.Type() != TypeObject {
+		return nil
+	}
+	k := v.Call("keys")
+	var o []string
+	size := k.Get("length").Int()
+	for i := 0; i < size; i++ {
+		o = append(o, k.Index(i).String())
+	}
+	return o
+}
+
+// returns true if value is not null or undefined.
+func Valid(v Value) bool {
+	if v == nil {
+		return false
+	}
+	if v.Type() == TypeUndefined {
+		return false
+	}
+	return v.Type() != TypeNull
+}
+
+// RemoveNode removes node from its parent if attached.
+func RemoveNode(node Value) {
+	parent := node.Get("parentNode")
+	if Valid(parent) {
+		parent.Call("removeChild", node)
+	}
+}
+
+// Element is an alias for the dom node.
+type Element Value
+
+// HasProperty returns true if e has property.
+func HasProperty(e Element, v string) bool {
+	return e.Call("hasOwnProperty", v).Bool()
+}
+
+// IsEqual returns true if the ywo elements are equal
+func IsEqual(a, b Element) bool {
+	return a.Call("isEqualNode", b).Bool()
 }
 
 // Templater is an interface for describing components with xml like markup. The
@@ -316,6 +393,9 @@ func (q *queuedRender) rerender() {
 		}
 	}
 }
+
+// CallbackGenerator is a function that returns callbacks.
+type CallbackGenerator func(fn func([]Value)) Callback
 
 // Vected this is the ultimate struct that ports preact to work with go/was.
 // This is not a direct port, the two languages are different. Although some
@@ -717,4 +797,127 @@ func (v *Vected) Register(name string, cmp Component) {
 		v.components = make(map[string]Component)
 	}
 	v.components[name] = cmp
+}
+
+// CreateNode creates a dom element.
+func (v *Vected) CreateNode(name string) Element {
+	node := v.Document.Call("createElement", name)
+	node.Set("normalizedNodeName", name)
+	return node
+}
+
+// CreateSVGNode creates svg dom element.
+func (v *Vected) CreateSVGNode(doc Value, name string) Element {
+	node := v.Document.Call("createElementNS", svg, name)
+	node.Set("normalizedNodeName", name)
+	return node
+}
+
+// SetAccessor Set a named attribute on the given Node, with special behavior
+// for some names and event handlers. If `value` is `null`, the
+// attribute/handler will be removed.
+// node An element to mutate
+//
+// name The name/key to set, such as an event or attribute name
+// old The last value that was set for this name/node pair
+// value An attribute value, such as a function to be used as an event handler
+// isSVG Are we currently diffing inside an svg?
+func SetAccessor(gen CallbackGenerator, node Element, name string, old, val interface{}, isSVG bool) {
+	if name == "className" {
+		name = "class"
+	}
+	switch name {
+	case "class":
+		v := val
+		if v == nil {
+			v = ""
+		}
+		node.Set("className", v)
+	case "style":
+		style := node.Get("style")
+		switch e := val.(type) {
+		case string:
+			style.Set("cssText", e)
+		case map[string]string:
+			if o, ok := old.(map[string]string); ok {
+				for k := range o {
+					if _, ok := e[k]; !ok {
+						style.Set(k, "")
+					}
+				}
+			}
+			for k, v := range e {
+				style.Set(k, v)
+			}
+		}
+	case "dangerouslySetInnerHTML":
+		node.Set("innerHTML", val)
+	default:
+		switch {
+		case strings.HasPrefix(name, "on"):
+			useCapture := name != strings.TrimSuffix(name, "Capture")
+			name = eventName(name)
+			if ev, ok := val.(func([]Value)); ok {
+				cb := gen(ev)
+				if old == nil {
+					node.Call("addEventListener", name, cb, useCapture)
+					// To release resources allocated for the callback we keep track of of all
+					// callbacks added to this node.
+					//
+					// These can be later removed by calling the functions.
+					var release Callback
+					release = gen(func(args []Value) {
+						node.Call("removeEventListener", name, cb, useCapture)
+						cb.Release()
+						release.Release()
+					})
+					releaseList := node.Get("_listeners")
+					if releaseList.Type() == TypeUndefined {
+						node.Set("_listeners", make(map[string]interface{}))
+						releaseList = node.Get("_listeners")
+					}
+					releaseList.Set(name, release)
+				}
+			} else {
+				// If we don't supply the event call back it is the same as saying remove
+				// this event.
+				//
+				// We release the resources allocated for the event callback and free up the
+				// event reference by setting its value to undefined.
+				releaseList := node.Get("_listeners")
+				if Valid(releaseList) {
+					releaseList.Call(name)
+					releaseList.Set(name, "")
+				}
+			}
+		case name != "list" && name != "type" && !isSVG && HasProperty(node, name):
+			func() {
+				defer recover()
+				if val != nil {
+					node.Set(name, val)
+				} else {
+					node.Set(name, "")
+				}
+			}()
+			if (val == nil || !toBool(val)) && name != "spellcheck" {
+				node.Call("removeAttribute", name)
+			}
+		default:
+			//TODO handle namespace
+		}
+	}
+}
+
+func toBool(v interface{}) bool {
+	if v, ok := v.(bool); ok {
+		return v
+	}
+	return false
+}
+
+// eventName takes a props event name and returns a string suitable for
+// registering the event on the dom.
+func eventName(name string) string {
+	name = strings.ToLower(name)
+	return name[2:]
 }

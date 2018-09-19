1534d5b513b444705c1101848923d4e2fffa2141
gernest <geofreyernest@live.com>
2018-09-19 09:16:18 +0300

    start work on expression parsring

---
+++ expr/expr.go
@@ -0,0 +1,114 @@
+package expr
+
+import (
+	"bytes"
+	"fmt"
+	"go/ast"
+	"go/parser"
+	"go/printer"
+	"go/token"
+	"os"
+	"strings"
+)
+
+func Parse(src string) (ast.Expr, error) {
+	a, err := parser.ParseExpr(src)
+	if err != nil {
+		return nil, err
+	}
+	a = wrap(a)
+	fset := token.NewFileSet()
+	ast.Print(fset, a)
+	return a, nil
+}
+
+func wrap(args ...ast.Expr) ast.Expr {
+	return &ast.CallExpr{
+		Fun: &ast.SelectorExpr{
+			X: &ast.Ident{
+				Name: "fmt",
+			},
+			Sel: &ast.Ident{
+				Name: "Println",
+			},
+		},
+		Args: args,
+	}
+}
+
+func printExpr(a ast.Expr) {
+	printer.Fprint(os.Stdout, token.NewFileSet(), a)
+	fmt.Print("\n")
+}
+
+// Expression represent part of text that should be evaluated. Something between
+// { }
+//
+// example { 1+2 }
+type Expression struct {
+	Text  string
+	Plain bool
+}
+
+// ExtractExpressions given a src string. This will find text that is within
+// begin and end marker returning it as Expression.
+//
+// For text within begin,end marker Expression.Plain field will be set to false,
+// to preserve order of expression, parsing is done from left to right, and any
+// other text will be returned in an expression whose Plain field is set to
+// true.
+//
+// Leading and trailing space is trimmed. So, It is not possible to reconstruct
+// original text from the returned expressions.
+//
+// Note that the expression must be valid go expressions.
+func ExtractExpressions(src string, begin, end rune) (result []Expression, err error) {
+	var buf bytes.Buffer
+	line, col, count := 0, 0, 0
+	for _, v := range src {
+		switch v {
+		case begin:
+			if buf.Len() > 0 {
+				txt := strings.TrimSpace(buf.String())
+				if txt != "" {
+					result = append(result, Expression{
+						Text:  txt,
+						Plain: true,
+					})
+				}
+				buf.Reset()
+			}
+			if count != 0 {
+				err = fmt.Errorf("unexpected %s at %d:%d", string(v), line, col)
+				return
+			}
+			count++
+			col++
+		case end:
+			result = append(result, Expression{
+				Text: buf.String(),
+			})
+			buf.Reset()
+			count--
+			col++
+		default:
+			switch v {
+			case '\n', '\r':
+				line++
+			default:
+				col++
+			}
+			buf.WriteRune(v)
+		}
+	}
+	if buf.Len() > 0 {
+		txt := strings.TrimSpace(buf.String())
+		if txt != "" {
+			result = append(result, Expression{
+				Text:  txt,
+				Plain: true,
+			})
+		}
+	}
+	return
+}
---
+++ expr/expr_test.go
@@ -0,0 +1,65 @@
+package expr
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestExpression(t *testing.T) {
+	sample := []struct {
+		src string
+	}{
+		{src: `func(){}`},
+	}
+	for _, v := range sample {
+		e, err := Parse(v.src)
+		if err != nil {
+			t.Error(err)
+		}
+		printExpr(e)
+	}
+	t.Error("yay")
+}
+
+func TestExtractExpression(t *testing.T) {
+	sample := []struct {
+		src    string
+		expect []Expression
+	}{
+		{"{a}{b}", []Expression{
+			{Text: "a", Plain: false},
+			{Text: "b", Plain: false},
+		}},
+		// Ignore white space
+		{" {a} {b} ", []Expression{
+			{Text: "a", Plain: false},
+			{Text: "b", Plain: false},
+		}},
+
+		// strings
+		{"hello", []Expression{
+			{Text: "hello", Plain: true},
+		}},
+
+		// string with expr
+		{"hello{a}", []Expression{
+			{Text: "hello", Plain: true},
+			{Text: "a", Plain: false},
+		}},
+
+		// string with expr and space
+		{"hello, world {a}", []Expression{
+			{Text: "hello, world", Plain: true},
+			{Text: "a", Plain: false},
+		}},
+	}
+	for _, v := range sample {
+		e, err := ExtractExpressions(v.src, '{', '}')
+		if err != nil {
+			t.Error(err)
+		}
+		if !reflect.DeepEqual(e, v.expect) {
+			t.Errorf("expected %v got %v", v.expect, e)
+		}
+	}
+}
